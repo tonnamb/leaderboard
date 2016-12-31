b820b30d30dfe254141a8de8621ffb5dde655624
Tonnam Balankura <tonnam.balankura@gmail.com>
2016-12-31 08:23:19 +0700

    complete functionality and user story

--- src/UserTable.js
+++ src/UserTable.js
@@ -7,21 +7,41 @@ class UserTable extends Component {
 
 	constructor(props) {
   	  super(props);
-  	  this.state = {users: [{"username":"loading","img":"","alltime":0,"recent":0}]};
+  	  this.state = {users: [{"username":"loading","img":"","alltime":0,"recent":0}], recent: "clicked", alltime: "not-clicked"};
+
+  	  this.clickRecent = this.clickRecent.bind(this);
+  	  this.clickAlltime = this.clickAlltime.bind(this);
+  	  this.loadData = this.loadData.bind(this);
   	}
 	
   	componentDidMount() {
-  	  let _this = this;
-  	  this.serverRequest =
-  	    axios
-  	      .get("https://fcctop100.herokuapp.com/api/fccusers/top/recent")
-  	      .then(function(result) {
-  	        _this.setState({users: result.data});
-  	      })
+  	  this.loadData("https://fcctop100.herokuapp.com/api/fccusers/top/recent");
   	}
 	
   	componentWillUnmount() {
   	  this.serverRequest.abort();
+  	}
+
+  	loadData(url) {
+  	  let _this = this;
+  	  this.serverRequest =
+  	    axios
+  	      .get(url)
+  	      .then(function(result) {
+  	        _this.setState({users: result.data});
+  	      })
+  	}
+
+  	clickRecent(e) {
+  	  e.preventDefault();
+  	  this.loadData("https://fcctop100.herokuapp.com/api/fccusers/top/recent");
+  	  this.setState({recent: "clicked", alltime: "not-clicked"});
+  	}
+
+  	clickAlltime(e) {
+  	  e.preventDefault();
+  	  this.loadData("https://fcctop100.herokuapp.com/api/fccusers/top/alltime");
+  	  this.setState({recent: "not-clicked", alltime: "clicked"});
   	}
 
 	render() {
@@ -40,8 +60,8 @@ class UserTable extends Component {
     		    <tr>
     		      <th>#</th>
     		      <th>Camper Name</th>
-    		      <th>Points in past 30 days</th>
-    		      <th>All time points</th>
+    		      <th className={this.state.recent}><a href="#" onClick={this.clickRecent}>Points in past 30 days</a></th>
+    		      <th className={this.state.alltime}><a href="#" onClick={this.clickAlltime}>All time points</a></th>
     		    </tr>
     		  </thead>
     		  <tbody>
