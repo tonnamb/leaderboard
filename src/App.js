55104c27a7851b61075cb6b997fc5db47fa501f3
Tonnam Balankura <tonnam.balankura@gmail.com>
2016-12-31 07:52:40 +0700

    move state to usertable

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
 import React, { Component } from 'react';
 import UserTable from './UserTable.js';
-import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {users: [{"username":"loading","img":"","alltime":0,"recent":0}]};
-  }
-
-  componentDidMount() {
-    let _this = this;
-    this.serverRequest =
-      axios
-        .get("https://fcctop100.herokuapp.com/api/fccusers/top/recent")
-        .then(function(result) {
-          _this.setState({users: result.data});
-        })
-
-  }
-
-  componentWillUnmount() {
-    this.serverRequest.abort();
-  }
 
   render() {
 
     return (
       <div className="App">
-        <UserTable users={this.state.users}/>
+        <UserTable />
       </div>
     );
   }
--- src/UserTable.js
+++ src/UserTable.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import UserRow from './UserRow.js';
 import './UserTable.css';
 
 class UserTable extends Component {
 
+	constructor(props) {
+  	  super(props);
+  	  this.state = {users: [{"username":"loading","img":"","alltime":0,"recent":0}]};
+  	}
+	
+  	componentDidMount() {
+  	  let _this = this;
+  	  this.serverRequest =
+  	    axios
+  	      .get("https://fcctop100.herokuapp.com/api/fccusers/top/recent")
+  	      .then(function(result) {
+  	        _this.setState({users: result.data});
+  	      })
+  	}
+	
+  	componentWillUnmount() {
+  	  this.serverRequest.abort();
+  	}
+
 	render() {
 
 		var rows = [];
-		this.props.users.forEach(function(useri, i) {
+		this.state.users.forEach(function(useri, i) {
 			rows.push(<UserRow user={useri} rank={i+1} key={i+1}/>);
 		});
 
